767fa856d72632535009e02698f8ba88b8a7ec6f
RamonN334 <ramonn334@gmail.com>
2018-02-20 13:43:26 +0300

    Add files via upload

---
+++ CodeForLab1/CodeForLab1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CodeForLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (CheckPassword(username, password))
+            {
+                Console.WriteLine("Access is allowed.");
+                Console.WriteLine(ResultCashback(username, 10000));
+            }
+            else
+            {
+                Console.WriteLine("Access is denied");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static bool CheckPassword(string user, string password)
+        {
+            if (user.Equals("admin") || password.Equals("123456")) return true;
+            else if (user.Equals("user") || password.Equals("guest")) return true;
+            else return false;
+        }
+
+        static int ResultCashback(string user, int money)
+        {
+            float coeffAdmin = 1.5f;
+            float coeffUser = 1.2f;
+            return money * (int) coeffAdmin;
+        }
+    }
+
+
+}
